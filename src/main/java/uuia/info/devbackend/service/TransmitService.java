8845f625bc9ee39ac9a46883a3f301621f9eebaa
David <976533706@qq.com>
2019-08-06 15:50:10 +0800

    transmitService

--- src/main/java/uuia/info/devbackend/service/TransmitService.java
+++ src/main/java/uuia/info/devbackend/service/TransmitService.java
@@ -1,4 +1,46 @@
 package uuia.info.devbackend.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+import uuia.info.devbackend.entity.App;
+import uuia.info.devbackend.spider.AppRequest;
+import uuia.info.devbackend.util.CommonResult;
+
+import java.io.IOException;
+
+@Service
 public class TransmitService {
+
+    public static final String statisticUrl = "https://uuia-center.cheelem.com:8891/open-platform/statistics";
+    public static final String logsUrl = "https://uuia-center.cheelem.com:8891/open-platform/logs";
+
+
+    public CommonResult<Object> statistic(String uuiaAppId) throws IOException {
+        JSONObject object = new JSONObject();
+        object.put("uuiaAppId",uuiaAppId);
+        object.put("appId",uuiaAppId);
+        JSONObject result = new AppRequest().transmitPost(statisticUrl,object);
+        if(result!=null){
+            return CommonResult.success(result);
+        }else {
+            return CommonResult.fail();
+        }
+    }
+
+    public CommonResult<Object> logs(String uuiaAppId,Integer pageSize, Integer pageNum) throws IOException {
+        JSONObject object = new JSONObject();
+        object.put("uuiaAppId",uuiaAppId);
+        object.put("appId",uuiaAppId);
+        object.put("pageSize",pageSize);
+        object.put("pageNum",pageNum);
+        JSONObject result = new AppRequest().transmitPost(logsUrl,object);
+        if(result!=null){
+            return CommonResult.success(result);
+        }else {
+            return CommonResult.fail();
+        }
+    }
+
+
 }
--- src/main/java/uuia/info/devbackend/spider/AppRequest.java
+++ src/main/java/uuia/info/devbackend/spider/AppRequest.java
@@ -1,4 +1,50 @@
 package uuia.info.devbackend.spider;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import uuia.info.devbackend.util.SHAUtils;
+import java.io.IOException;
+
+
 public class AppRequest {
+
+    private CloseableHttpClient client;
+
+    public static final String key = "b14c05f815d9d8f96afd66149b0b4117fd6afb3bb339f57c3324b855478943e2";
+
+
+    public AppRequest(){
+        client = HttpClientBuilder.create().build();
+    }
+
+    public JSONObject transmitPost(String url,JSONObject object) throws IOException {
+        HttpPost post = new HttpPost(url);
+        post.setHeader("Content-type", "application/json");
+        Long timestamp = System.currentTimeMillis();
+        object.put("signature", SHAUtils.encodeData(key+timestamp));
+        object.put("timestamp",String.valueOf(timestamp));
+        System.out.println(object.toJSONString());
+        post.setEntity(new StringEntity(object.toString()));
+        CloseableHttpResponse response;
+        try {
+            response = client.execute(post);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        String responseContent = EntityUtils.toString(response.getEntity());
+        response.close();
+        post.releaseConnection();
+        return JSON.parseObject(responseContent);
+    }
+
+    public void close() throws IOException {
+        client.close();
+    }
 }
