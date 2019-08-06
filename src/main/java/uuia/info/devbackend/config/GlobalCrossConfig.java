dcf3ccc3bc4eb4646d5e28211b0ca05752573e19
Raven <raven.xu98@gmail.com>
2019-08-06 23:41:45 +0800

    修复跨域配置错误

---
+++ src/main/java/uuia/info/devbackend/config/GlobalCrossConfig.java
@@ -0,0 +1,33 @@
+package uuia.info.devbackend.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 全局支持跨域请求配置
+ * @Author: Raven
+ * @Date: 2019/8/2 10:20 AM
+ */
+@Configuration
+public class GlobalCrossConfig {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
